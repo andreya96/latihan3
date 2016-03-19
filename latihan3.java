f1f4a8453e289ba7b646acd0990c8183a75bea23
buncis mamen <buncismamen@gmail.com>
2016-03-19 19:03:22 +0700

    add starter code

---
+++ latihan3.java
@@ -0,0 +1,9 @@
+public class latihan3 {
+
+    public static void main(String[] args) {
+          // Prints "hello, World" to the terminal window.
+          System.out.println("Hello, World");
+    }
+}
+
+//RUbah agar hellow world berubah menjadi hello alex
