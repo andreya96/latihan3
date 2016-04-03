448d25676439a07785cfdfe96369861bd7ca9af7
buncismamen <buncismamen@gmail.com>
2016-04-03 19:54:07 +0700

    update latihan3.java

--- latihan3.java
+++ latihan3.java
@@ -2,7 +2,7 @@ public class latihan3 {
 
     public static void main(String[] args) {
           // Prints "hello, World" to the terminal window.
-          System.out.println("Hello, World");
+          System.out.println("hello, World");
     }
 }
 
